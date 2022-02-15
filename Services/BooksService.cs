17aa6b5bbee71c7d73bdc8af89859f38c1eddba6
Daniel Hopson <hdhopson88@yahoo.com>
2022-02-15 14:05:41 -0600

    Adds Git Ignore

--- Services/BooksService.cs
+++ Services/BooksService.cs
@@ -7,7 +7,7 @@ namespace BookStoreApi.Services;
 
 public class BooksService : IBooksService
 {
-    private readonly IMongoCollection<Book> _booksCollection;
+    private readonly IMongoCollection<Book> booksCollection;
 
     public BooksService(
         IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
@@ -18,22 +18,22 @@ public class BooksService : IBooksService
         var mongoDatabase = mongoClient.GetDatabase(
             bookStoreDatabaseSettings.Value.DatabaseName);
 
-        _booksCollection = mongoDatabase.GetCollection<Book>(
+        booksCollection = mongoDatabase.GetCollection<Book>(
             bookStoreDatabaseSettings.Value.BooksCollectionName);
     }
 
     public async Task<List<Book>> GetAsync() =>
-        await _booksCollection.Find(_ => true).ToListAsync();
+        await booksCollection.Find(_ => true).ToListAsync();
 
     public async Task<Book?> GetAsync(string id) =>
-        await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        await booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
     public async Task CreateAsync(Book newBook) =>
-        await _booksCollection.InsertOneAsync(newBook);
+        await booksCollection.InsertOneAsync(newBook);
 
     public async Task UpdateAsync(string id, Book updatedBook) =>
-        await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+        await booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
 
     public async Task RemoveAsync(string id) =>
-        await _booksCollection.DeleteOneAsync(x => x.Id == id);
+        await booksCollection.DeleteOneAsync(x => x.Id == id);
 }
